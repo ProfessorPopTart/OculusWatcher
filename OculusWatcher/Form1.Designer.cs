28f849a0383c5ff71ef082827f272dd07517be62
Professorpoptart <professorpoptart@gmail.com>
2016-05-18 16:04:03 -0400

    Base Commit

---
+++ OculusWatcher/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace OculusWatcher
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tbxFrom = new System.Windows.Forms.TextBox();
+            this.tbxPwd = new System.Windows.Forms.TextBox();
+            this.tbxTo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxUrl = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(0, 101);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1149, 477);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tbxFrom
+            // 
+            this.tbxFrom.Location = new System.Drawing.Point(15, 63);
+            this.tbxFrom.Name = "tbxFrom";
+            this.tbxFrom.Size = new System.Drawing.Size(188, 20);
+            this.tbxFrom.TabIndex = 1;
+            this.tbxFrom.Text = "[sender email]@gmail.com";
+            // 
+            // tbxPwd
+            // 
+            this.tbxPwd.Location = new System.Drawing.Point(209, 63);
+            this.tbxPwd.Name = "tbxPwd";
+            this.tbxPwd.PasswordChar = '*';
+            this.tbxPwd.Size = new System.Drawing.Size(174, 20);
+            this.tbxPwd.TabIndex = 2;
+            this.tbxPwd.Text = "password";
+            // 
+            // tbxTo
+            // 
+            this.tbxTo.Location = new System.Drawing.Point(389, 63);
+            this.tbxTo.Name = "tbxTo";
+            this.tbxTo.Size = new System.Drawing.Size(397, 20);
+            this.tbxTo.TabIndex = 3;
+            this.tbxTo.Text = "[to email]@gmail.com, [number]@txt.att.net";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(806, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Test Notification";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Gmail account to send email through";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(386, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "send to:";
+            // 
+            // tbxUrl
+            // 
+            this.tbxUrl.Location = new System.Drawing.Point(15, 24);
+            this.tbxUrl.Name = "tbxUrl";
+            this.tbxUrl.Size = new System.Drawing.Size(771, 20);
+            this.tbxUrl.TabIndex = 7;
+            this.tbxUrl.Text = "http://www.bestbuy.com/site/oculus-rift-with-luckys-tale-game-black/4932000.p?id=" +
+    "bb4932000&skuId=4932000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(400, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "URL to look at, as soon as \"Sold Out Online\" button is gone it will send notifica" +
+    "tions";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 578);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxUrl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbxTo);
+            this.Controls.Add(this.tbxPwd);
+            this.Controls.Add(this.tbxFrom);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "frmMain";
+            this.Text = "Oculus Watcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox tbxFrom;
+        private System.Windows.Forms.TextBox tbxPwd;
+        private System.Windows.Forms.TextBox tbxTo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxUrl;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ OculusWatcher/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OculusWatcher
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            timer1_Tick(null, null);
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            bool soldOut = false;
+            var block = webBrowser1.Document.GetElementById("priceblock-wrapper-wrapper");
+            if (block == null)
+                return;
+
+            HtmlElementCollection spans = block.GetElementsByTagName("span");
+            foreach (HtmlElement span in spans)
+            {
+                if (span.InnerText == "Loading")
+                    return;
+
+                if (span.InnerText == "Sold Out Online")
+                {
+                    webBrowser1.DocumentCompleted -= webBrowser1_DocumentCompleted;
+                    soldOut = true;
+                }
+            }
+
+            if (!soldOut)
+            {
+                webBrowser1.DocumentCompleted -= webBrowser1_DocumentCompleted;
+                Notify("In Stock at Best Buy!");
+            }
+            else
+                this.Text = "Oculus Watcher - Out of Stock as of " + DateTime.Now;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            webBrowser1.DocumentCompleted += webBrowser1_DocumentCompleted;
+            webBrowser1.Navigate(tbxUrl.Text);
+        }
+
+        private bool Notify(string msg)
+        {
+            this.Text = "Oculus Watcher - " + msg;
+            var message = new MailMessage();
+            message.From = new MailAddress(tbxFrom.Text);
+
+            string[] emails = tbxTo.Text.Split(',');
+            foreach(string email in emails)
+                message.To.Add(new MailAddress(email.Trim()));
+
+            message.Subject = "Oculus";
+            message.Body = msg + " " + tbxUrl.Text;
+            message.BodyEncoding = Encoding.UTF8;
+
+            var client = new SmtpClient();
+            client.Host = "Smtp.Gmail.com";
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.Timeout = 10000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential(tbxFrom.Text, tbxPwd.Text);
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to send message. Make sure you allow less secure apps for the gmail account and try again.");
+                Process.Start("chrome.exe", "https://www.google.com/settings/security/lesssecureapps");
+                return false;
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Notify("Oculus Test Message"))
+                MessageBox.Show("Email sent successfully!");
+        }
+    }
+}
